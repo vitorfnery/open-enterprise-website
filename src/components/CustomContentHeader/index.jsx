f79ac9cd4e5e239b2a3ef9954fba8d253f119433
Vítor F. Nery <vitorfelipenery@gmail.com>
2023-06-07 11:58:25 -0300

    make hero section responsive

--- src/components/CustomContentHeader/index.jsx
+++ src/components/CustomContentHeader/index.jsx
@@ -3,11 +3,11 @@ const CustomContentHeader = ({ title, content }) => {
     <div
       className="
                   flex flex-col mx-auto lg:ml-[18.958333333%] lg:mr-[18.888888889%] 
-                  mt-20 md:mt-30 lg:mt-40 
+                  mt-14 sm:mt-20 md:mt-30 lg:mt-40 
                   font-normal text-dark-charcoal text-center
                 "
     >
-      <h3 className="text-3xl lg:text-5xl leading-[64px] font-dmSerif mb-6">
+      <h3 className="text-3xl lg:text-5xl leading-10  sm:leading-[64px] font-dmSerif mb-6">
         {title}
       </h3>
       <p
--- src/containers/Hero/index.jsx
+++ src/containers/Hero/index.jsx
@@ -9,23 +9,34 @@ const Hero = () => {
       className="
                   w-full grid grid-cols-1 lg:grid-cols-2 
                   justify-items-center lg:justify-items-start
-                  mt-[3%] lg:mt-[8%] 
-                  gap-y-20
+                  
+                  mt-[15%] sm:mt-[8%] 
+                  gap-y-0 lg:gap-y-20
                   lg:gap-x-10
                 "
     >
       <div className="w-4/5 lg:w-full">
-        <h1 className="font-dmSerif font-normal text-3xl sm:text-5xl lg:text-[52px] text-dark-charcoal leading-[50px] lg:leading-[60px]">
+        <h1
+          className="
+                    font-dmSerif font-normal text-dark-charcoal 
+                    text-3xl sm:text-5xl lg:text-[52px]  
+                    leading-9 sm:leading-[50px] lg:leading-[60px]
+                    "
+        >
           {title}
         </h1>
-        <p className="pt-11 pb-10 font-figtree font-normal text-base">
+        <p className="pt-5 pb-6 md:pt-11 md:pb-10 font-figtree font-normal text-base">
           {content}
         </p>
         <div className="flex justify-center md:block">
           <CustomButton content={btn} styleClass={"hero__btn"} />
         </div>
       </div>
-      <img className="-mt-[20%] mx-auto z-0" src={HeroImg} alt="Hero Image" />
+      <img
+        className="lg:-mt-[20%] mx-auto z-0 scale-[0.85] lg:scale-100"
+        src={HeroImg}
+        alt="Hero Image"
+      />
     </div>
   );
 };
